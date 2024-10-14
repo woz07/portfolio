0d5e93ae0c59e61856e9839c763dc01a1a042e9b
woz07 <muhsameer9@gmail.com>
2024-10-14 23:08:34 +0100

    TODO: index.js & certificates

---
+++ index.js
@@ -0,0 +1,9 @@
+const colours = ["#C5E5A5"];
+/**
+ * TODO:
+ * Change all highlights to a single highlight class and then use that to get all elements
+ * that require a different colour and change it randomly using the given colors already besides
+ * yellow, which is default.
+ * 
+ * ADD MORE CERTIFICATES
+ */
